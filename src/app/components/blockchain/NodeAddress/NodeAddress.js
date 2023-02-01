edfa7f11fdf4b493f5a40a751d63e49c996bdb3f
David Tai <dtaipublic@gmail.com>
2023-02-01 11:00:59 -0600

    Update NodeAddress.js

--- src/app/components/blockchain/NodeAddress/NodeAddress.js
+++ src/app/components/blockchain/NodeAddress/NodeAddress.js
@@ -1,1 +1,1 @@
-export const NODE_ADDRESS = 'https://casperlabs-proxy-server.herokuapp.com/http://44.208.234.65:7777/rpc'
+export const NODE_ADDRESS = 'https://proxy.casperswap.xyz/?url=http://44.208.234.65:7777/rpc'
